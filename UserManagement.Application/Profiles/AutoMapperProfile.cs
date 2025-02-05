43e08c42dd2cb7f58473995b04c94811a75054bb
Vishwajeet Velapurkar <vvelapurkar@altimetrik.com>
2025-02-05 15:49:23 +0530

    Auto mapper updates

--- UserManagement.Application/Profiles/AutoMapperProfile.cs
+++ UserManagement.Application/Profiles/AutoMapperProfile.cs
@@ -3,18 +3,17 @@ using UserManagement.Application.Dtos;
 using UserManagement.Application.Extensions;
 using UserManagement.Application.Models;
 
-namespace UserManagement.Application.Profiles
+namespace UserManagement.Application.Profiles;
+
+public class AutoMapperProfile : Profile
 {
-    public class AutoMapperProfile : Profile
+    public AutoMapperProfile()
     {
-        public AutoMapperProfile()
-        {
-            CreateMap<Users, UserDto>()
-                .ForMember(d => d.Created, opt => opt.MapFrom(src => src.CreatedTimestamp.ToDateTimeOrNow()))
-                .ForMember(d => d.Id, opt => opt.MapFrom(src => new Guid(src.Id)))
-                .ReverseMap()
-                .ForMember(d => d.CreatedTimestamp, opt => opt.MapFrom(src => src.Created.ToUnixTimestamp()))
-                .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id.ToString()));
-        }
+        CreateMap<Users, UserDto>()
+            .ForMember(d => d.Created, opt => opt.MapFrom(src => src.CreatedTimestamp.ToDateTimeOrNow()))
+            .ForMember(d => d.Id, opt => opt.MapFrom(src => new Guid(src.Id)))
+            .ReverseMap()
+            .ForMember(d => d.CreatedTimestamp, opt => opt.MapFrom(src => src.Created.ToUnixTimestamp()))
+            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id.ToString()));
     }
 }
