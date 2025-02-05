bbab9ca2369af1811848e8a7cf7d803f29e89c9e
Vishwajeet Velapurkar <vvelapurkar@altimetrik.com>
2025-02-05 15:44:41 +0530

    Added Auto mapper

---
+++ UserManagement.Application/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using UserManagement.Application.Dtos;
+using UserManagement.Application.Extensions;
+using UserManagement.Application.Models;
+
+namespace UserManagement.Application.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Users, UserDto>()
+                .ForMember(d => d.Created, opt => opt.MapFrom(src => src.CreatedTimestamp.ToDateTimeOrNow()))
+                .ForMember(d => d.Id, opt => opt.MapFrom(src => new Guid(src.Id)))
+                .ReverseMap()
+                .ForMember(d => d.CreatedTimestamp, opt => opt.MapFrom(src => src.Created.ToUnixTimestamp()))
+                .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id.ToString()));
+        }
+    }
+}
